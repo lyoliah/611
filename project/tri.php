65ab434041128bed9d0b71dcf40b5e0b819a56d8
lyoliah <136178915+lyoliah@users.noreply.github.com>
2023-10-04 20:13:52 +0200

    be happy
    be silly

---
+++ project/tri.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YOU</title>
+    <style>
+        body{
+            margin: 100px 0 0 0;
+            background-image: url(https://i.quotev.com/ubq4dxuw25na.jpg);
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-attachment: fixed; 
+        }
+    *, *::before, *::after {
+    box-sizing: border-box;
+    }
+    .hoh{
+        color: yellow;
+    }
+    .text-field {
+    margin-bottom: 1rem;
+        }
+        /* стили для label */
+        .text-field__label {
+        display: block;
+        margin-bottom: 0.25rem;
+        }
+        /* стили для input */
+        .text-field__input {
+        display: block;
+        width: 100%;
+        height: calc(2.25rem + 2px);
+        padding: 0.375rem 0.75rem;
+        font-family: inherit;
+        font-size: 1rem;
+        font-weight: 400;
+        line-height: 1.5;
+        color: yellow;
+        background-color: blue;
+        background-clip: padding-box;
+        border: 1px solid #bdbdbd;
+        border-radius: 0.25rem;
+        transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+        }
+        .text-field__input::placeholder {
+        color: yellow;
+        opacity: 0.4;
+        }
+    </style>
+</head>
+<body class = "hoh">
+    
+    <div class="text-field">
+        <label class="text-field__label" for="username">ФИО</label>
+        <input class="text-field__input" type="text" name="username" id="username" placeholder="Username">
+    </div>
+    <div class="text-field">
+        <label class="text-field__label" for="username">Возвраст</label>
+        <input class="text-field__input" type="text" name="username" id="username" placeholder="06.11.2007">
+    </div>
+        
+    <div class="text-field">
+        <label class="text-field__label" for="username">Страна</label>
+        <input class="text-field__input" type="text" name="username" id="username" placeholder="Ukraine">
+    </div>
+        
+    <div class="text-field">
+        <label class="text-field__label" for="username">Город</label>
+        <input class="text-field__input" type="text" name="username" id="username" placeholder="Poltava">
+    </div>
+    <div class="text-field">
+        <label class="text-field__label" for="username">Телефон</label>
+        <input class="text-field__input" type="text" name="username" id="username" placeholder="+380969690145">
+    </div>
+    <div class="text-field">
+        <label class="text-field__label" for="username">Хобби</label>
+        <input class="text-field__input" type="text" name="username" id="username" placeholder="Fall">
+    </div>
+    <div class="text-field">
+        <label class="text-field__label" for="username">Страхи</label>
+        <input class="text-field__input" type="text" name="username" id="username" placeholder="You">
+    </div>
+    <div class="text-field">
+        <label class="text-field__label" for="username">Друзья</label>
+        <input class="text-field__input" type="text" name="username" id="username" placeholder="Ты">
+    </div>
+    <div class="text-field">
+        <label class="text-field__label" for="username">Номера друзей</label>
+        <input class="text-field__input" type="text" name="username" id="username" placeholder="Ty">
+    </div>
+    <div class="text-field">
+        <label class="text-field__label" for="username">Номера родителей</label>
+        <input class="text-field__input" type="text" name="username" id="username" placeholder="Du">
+    </div>
+    <div class="text-field">
+        <label class="text-field__label" for="username">Что тебя беспокоит?</label>
+        <input class="text-field__input" type="text" name="username" id="username" placeholder="Vos">
+    </div>
+    <div class="text-field">
+        <label class="text-field__label" for="username">Ты хочешь об этом поговорить?</label>
+        <input class="text-field__input" type="text" name="username" id="username" placeholder="Kai">
+    </div>
+    <p>Ты стесняешься своего имени?</p>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    <?php
+    $username = getenv('USERNAME') ?: getenv('USER');
+    echo $username; // e.g. root or www-data
+    ?>
+    
+</body>
+</html>
